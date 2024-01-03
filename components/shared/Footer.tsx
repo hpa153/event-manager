e1d9b794ab9a64fb33b9be891b8d06cb4f314bea
Phuong Anh Hoang <phuonganhhoang.153@gmail.com>
2024-01-02 20:09:59 -0500

    initial app structure

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Evently logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>&copy; 2023 Evently. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ lib/db/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  console.log(cached);
+  try {
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URI, {
+        dbName: "evently",
+        bufferCommands: false,
+      });
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+  } catch (error) {
+    console.log(error);
+  }
+};
